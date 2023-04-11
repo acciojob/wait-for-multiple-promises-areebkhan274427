eab6cf2482adbd9a35f3abbc06f36308fb745c13
Areeb Khan <55325380+areebkhan274427@users.noreply.github.com>
2023-04-12 02:03:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
 //your JS code here. If required.
+
+const table = document.getElementById('output');
+const loadingRow = document.querySelector('tbody > tr');
+
+var promises=[];
+
+for(let i=1;i<=3;i++){
+	const promise = new Promise((resolve,reject)=>{
+		const randomTime = Math.floor(Math.random() * (3000)) + 1000;
+		setTimeout(()=>{
+			resolve(randomTime/1000);
+		},randomTime)
+	})
+	promises.push(promise);
+}
+
+Promise.all(promises).then((result)=>{
+	table.removeChild(loadingRow);
+	for(let i=1;i<=3;i++){
+		const row = document.createElement('tr');
+		const proCol = document.createElement('td');
+		const timeCol = document.createElement('td');
+		
+		proCol.textContent = `Promise ${i}`;
+		timeCol.textContent = `${result[i-1]}`;
+
+		row.appendChild(proCol);
+		row.appendChild(timeCol);
+		table.appendChild(row);
+	}
+
+	const totalRow = document.createElement('tr');
+	const totalCol = document.createElement('td');
+	const totalTimeCol = document.createElement('td');
+
+	totalCol.textContent = 'Total';
+	totalTimeCol.textContent = `${
+		result.reduce((acc,val)=>{
+			return acc+val;
+		},0)
+	}`
+
+	totalRow.appendChild(totalCol);
+	totalRow.appendChild(totalTimeCol);
+	table.appendChild(totalRow);
+
+})
+
+
